92fb31995b5f910fc2155b9878bf0bb4f5d2242c
Kelsey H Yu <hangkelseyyu@gmail.com>
2023-10-13 15:27:07 -0500

    fix after review

--- where_advanced_exercises.sql
+++ where_advanced_exercises.sql
@@ -74,52 +74,63 @@ where first_name ='Irena'or first_name ='Vidya'or first_name ='Maya';
 -- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. What is the employee number of the top three results?
 select * 
 from employees
-where first_name in ('Irena', 'Vidya','Maya')
+where (first_name ='Irena'or first_name ='Vidya'or first_name ='Maya') #add parathesis when "or" and "and" present together
 and gender = 'M';
 #10200,10397,10821#
 
 -- Find all unique last names that start with 'E'.
-select * 
+select distinct* 
 from employees
-where last_name like ('E%');
+where last_name like 'E%';
 -- Find all unique last names that start or end with 'E'.
-select * 
+select distinct* 
 from employees
-where last_name like ('%E')
-or last_name like ('E%');
+where last_name like '%E'
+or last_name like 'E%';
 -- Find all unique last names that end with E, but does not start with E?
-select * 
+select distinct * 
 from employees
-where last_name like ('%E')
-and last_name not like ('E%');
+where last_name like '%E'
+and last_name not like 'E%';
 
 -- Find all unique last names that start and end with 'E'.
-select * 
+select distinct * 
 from employees
-where last_name like ('%E')
-and last_name like ('E%');
+where last_name like 'E%E';
 -- Find all current or previous employees hired in the 90s. Enter a comment with the top three employee numbers.
 select * 
 from employees
-where hire_date between '1990-01-01' and '1999-12-31'
-order by hire_date desc;
+where hire_date between '1990-01-01' and '1999-12-31';
+#10008, 10011, 10012#
+-- or
+select * 
+from employees
+where hire_date like '199%';
+-- or
+select * 
+from employees
+where hire_date between 19900101 and 19991231;
+
 -- Find all current or previous employees born on Christmas. Enter a comment with the top three employee numbers.
 select * 
 from employees
 where birth_date like '%12-25';
 #10078,10115,10261#
+
 -- Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with the top three employee numbers.
 select * 
 from employees
 where hire_date between '1990-01-01' and '1999-12-31'
 and birth_date like '%12-25';
 #10261,10438,10681#
+
 -- Find all unique last names that have a 'q' in their last name.
-select * 
+select distinct last_name
 from employees
 where last_name like '%q%';
+
 -- Find all unique last names that have a 'q' in their last name but not 'qu'.
-select * 
+select distinct last_name
 from employees
 where last_name like '%q%'
 and last_name not like '%qu%';
