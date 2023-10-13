03b285f2a55d1b631bebd3f6ea7336faab316a6e
Kelsey H Yu <hangkelseyyu@gmail.com>
2023-10-13 15:31:43 -0500

    first edit

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -48,12 +48,14 @@ from employees
 where first_name in ('Irena', 'Vidya','Maya')
 order by first_name, last_name;
 #Irena Acton, Vidya Zweizig#
+
 -- Find all employees with first names 'Irena', 'Vidya', or 'Maya', and order your results returned by last name and then first name. In your comments, answer: What was the first and last name in the first row of the results? What was the first and last name of the last person in the table?
 select*
 from employees
 where first_name in ('Irena', 'Vidya','Maya')
 order by last_name, first_name;
 #Irena Acton, Maya Zyda#
+
 -- Write a query to find all employees whose last name starts and ends with 'E'. Sort the results by their employee number. Enter a comment with the number of employees returned, the first employee number and their first and last name, and the last employee number with their first and last name.
 select*
 from employees
@@ -61,6 +63,7 @@ where last_name like 'E%'
 and last_name like '%E'
 order by emp_no;
 #899 employees returned, Ramzi Erde, Tadahiro Erde#
+
 -- Write a query to find all employees whose last name starts and ends with 'E'. Sort the results by their hire date, so that the newest employees are listed first. Enter a comment with the number of employees returned, the name of the newest employee, and the name of the oldest employee.
 select*
 from employees
