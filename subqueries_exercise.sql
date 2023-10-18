91dd072690d52ddb70a0c4c5cc9002391ce0ca83
Kelsey H Yu <hangkelseyyu@gmail.com>
2023-10-18 11:18:08 -0500

    2nd edit

--- subqueries_exercise.sql
+++ subqueries_exercise.sql
@@ -1,2 +1,6 @@
 /*------------------ Subquery -----------------------*/
 /* - inner query/ subquery needs to be a full query
+     - scaler value: numeric, single value 
+     - can return a column or a table
+     - inner query will be execute first,then outer query
+     - can be in 'select' 'from' or 'where' (most common)
