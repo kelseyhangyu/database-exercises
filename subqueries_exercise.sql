3c5481da229ba320899a03f76702dd31bbe72744
Kelsey H Yu <hangkelseyyu@gmail.com>
2023-10-18 16:27:59 -0500

    finished exercise

--- subqueries_exercise.sql
+++ subqueries_exercise.sql
@@ -4,3 +4,18 @@
      - can return a column or a table
      - inner query will be execute first,then outer query
      - can be in 'select' 'from' or 'where' (most common)
+       -- if inner query returns a table instead a value, it needs a 'from' in front  */
+       
+############### Example ##############
+use employees;
+select database();
+show tables;
+-- example 1: find out the salaries that are higher than average salary
+select *
+from salaries
+where salary > 
+           ( select avg(salary)
+			from salaries);
+     
+
+     
