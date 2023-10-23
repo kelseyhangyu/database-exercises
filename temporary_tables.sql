2bdcc266391d63613f8ba1e6909fab6048682a58
Kelsey H Yu <hangkelseyyu@gmail.com>
2023-10-23 08:50:30 -0500

    added instructor notes

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -158,34 +158,116 @@ select * from payment;
 -- For this comparison, you will calculate the z-score for each salary. 
 -- In terms of salary, what is the best department right now to work for? The worst?
 use ursula_2336;
-#drop table dept_avg_salary;
-create temporary table dept_avg_salary as 
+
+create temporary table cur_avg_pay as 
 select avg(salary) as avg_salary, dept_name, dept_no
 from employees.salaries
 join employees.dept_emp using (emp_no)
 join employees.departments using (dept_no)
 where employees.salaries.to_date > now()
 group by dept_name;
-#drop table salary_zscore;
-create temporary table salary_zscore as
-   SELECT emp_no, salary,
-        (salary - (SELECT AVG(salary) FROM employees.salaries where to_date > now()))
+
+create temporary table dept_zscore as
+SELECT dept_name,
+(salary - (SELECT AVG(salary) FROM employees.salaries where to_date > now()))
         /
-        (SELECT stddev(salary) FROM employees.salaries where to_date > now()) AS zscore
-    FROM employees.salaries
-    WHERE to_date > now();
-    select * from salary_zscore;
+(SELECT stddev(salary) FROM employees.salaries where to_date > now()) 
+AS zscore
+FROM employees.salaries
+join employees.dept_emp using (emp_no)
+join employees.departments using (dept_no)
+WHERE salaries.to_date > now()
+;
 
-select emp_no, zscore,dept_name, avg_salary
+create temporary table overall as 
+select sum(pay) as overall_pay
+from employees.salaries
+;
 
+
+select dept_name, avg(zscore)
 from salary_zscore
 join employees.dept_emp using (emp_no)
 join employees.departments using (dept_no)
 join dept_avg_salary using (dept_name)
+group by dept_name
+order by avg(zscore) desc
 ;
-#-------------- Another way ----------------#
+
+# ------------------------ the instructor's way ------------------------- #
+-- Overall current salary stats
+select 
+	avg(salary), 
+    std(salary)
+from employees.salaries 
+where to_date > now();
+
+-- 72,012 overall average salary
+-- 17,310 overall standard deviation
 
 
+-- Saving my values for later... that's what variables do (with a name)
+-- Think about temp tables like variables
+use employees;
+
+drop table overall_aggregates;
+-- get overall std 
+create temporary table overall_aggregates as (
+    select 
+		avg(salary) as avg_salary,
+        std(salary) as overall_std
+    from employees.salaries  where to_date > now()
+);
+
+select * from overall_aggregates;
+
+
+-- Let's check out our current average salaries for each department
+-- If you see "for each" in the English for a query to build..
+-- Then, you're probably going to use a group by..
+-- want to compare each departments average salary to the overall std and overall avg salary
+
+drop table current_info;
+-- get current average salaries for each department
+create temporary table current_info as (
+    select 
+		dept_name, 
+        avg(salary) as department_current_average
+    from employees.salaries s
+    join employees.dept_emp de
+		on s.emp_no=de.emp_no and 
+        de.to_date > NOW() and 
+        s.to_date > NOW()
+    join employees.departments d
+		on d.dept_no=de.dept_no
+    group by dept_name
+);
+
+select * from current_info;
+
+
+-- add on all the columns we'll end up needing:
+alter table current_info add overall_avg float(10,2);
+alter table current_info add overall_std float(10,2);
+alter table current_info add zscore float(10,2);
+
+-- set the avg and std
+update current_info set overall_avg = (select avg_salary from overall_aggregates);
+update current_info set overall_std = (select overall_std from overall_aggregates);
+
+
+-- update the zscore column to hold the calculated zscores
+update current_info 
+set zscore = (department_current_average - overall_avg) / overall_std;
+
+
+select * from current_info;
+
+select 
+	max(zscore) as max_score
+from current_info 
+where max(zscore) = .97
+;
 
 # BONUS #
  -- Determine the overall historic average department average salary, the historic overall average, and the historic z-scores for salary. 
