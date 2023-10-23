c38b92c7113db81fdd029b6e59e6ec727b888bc3
Kelsey H Yu <hangkelseyyu@gmail.com>
2023-10-23 11:43:48 -0500

    added one to many examples

--- quiz_review.sql
+++ quiz_review.sql
@@ -2,3 +2,8 @@
 -- parathensis always indicate funcion
 -- foreign key review
 -- review many - to -many / one - to - many
+   -- one to many relationships are one employee to many salaries 
+   -- (one employee can have multiple different salaries, but a given salary always belongs to a single employee). 
+   -- An example of a many to many relationship is between employees and departments. 
+   -- An employee could have worked for many different departments, and a department can have many different employees.
+
