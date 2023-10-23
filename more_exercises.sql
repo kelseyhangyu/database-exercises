2ba0cbba34f152ff3f82e780b37157d5b8431390
Kelsey H Yu <hangkelseyyu@gmail.com>
2023-10-23 10:45:56 -0500

    created exercises

--- more_exercises.sql
+++ more_exercises.sql
@@ -1,30 +1,78 @@
 -- 1. SELECT statements
 
 # Select all columns from the actor table.
+use sakila;
+select * from actor;
 # Select only the last_name column from the actor table.
+select last_name from actor;
 # Select only the film_id, title, and release_year columns from the film table.
+select film_id, title, release_year from film;
 
 -- 2. DISTINCT operator
 
 # Select all distinct (different) last names from the actor table.
+select distinct last_name from actor;
 # Select all distinct (different) postal codes from the address table.
+select distinct postal_code from address;
 # Select all distinct (different) ratings from the film table.
+select distinct rating from film;
 
 -- 3. WHERE clause
 
 # Select the title, description, rating, and movie length columns from the films table that last 3 hours or longer.
+select title, description, rating, length 
+from film 
+where length >= 3;
+
 # Select the payment id, amount, and payment date columns from the payments table for payments made on or after 05/27/2005.
+select payment_id, amount, payment_date
+from payments
+where payment_date >= '05/27/2005';
+
 # Select the primary key, amount, and payment date columns from the payment table for payments made on 05/27/2005.
+select payment_id, amount, payment_date
+from payment
+where payment_date  >= '05/27/2005';
+
 # Select all columns from the customer table for rows that have last names beginning with "S" and first names ending with "N".
+select * 
+from customer
+where last_name like 'S%'
+and first_name like '%N';
+
 # Select all columns from the customer table for rows where the customer is inactive or has a last name beginning with "M".
+select *
+from customer
+where active = 'No'
+or last_name like 'M%';
+
 # Select all columns from the category table for rows where the primary key is greater than 4 and the name field begins with either "C", "S" or "T".
+select *
+from category
+where category_id > 4
+and name like 'c%' or 'S%' or 'T%';
+
 # Select all columns minus the password column from the staff table for rows that contain a password.
+select * #write down all columns except for password
+from staff 
+where password is not null; 
+
 # Select all columns minus the password column from the staff table for rows that do not contain a password.
+select * #write down all columns except for password
+where password is null;
 
 -- 4. IN operator
 
 # Select the phone and district columns from the address table for addresses in California, England, Taipei, or West Java.
+select phone, district
+from address
+where address in ('california','england','taipei','west java');
+
 # Select the payment id, amount, and payment date columns from the payment table for payments made on 05/25/2005, 05/27/2005, and 05/29/2005. (Use the IN operator and the DATE function, instead of the AND operator as in previous exercises.)
+select payment_id, amount, payment_date
+from payment
+where payment_date in ('05/25/2005', '05/27/2005','05/29/2005');
+
 # Select all columns from the film table for films rated G, PG-13 or NC-17.
 
 -- 5. BETWEEN operator
